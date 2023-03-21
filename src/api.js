f58e2e6937002551b38aed54b171a1bf65698c94
AndreiMuresan <andreicosminmuresan@gmail.com>
2023-03-21 16:13:29 +0200

    add relative path to backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  // baseURL: 'http://localhost:8080',
+  baseURL: window.location.protocol + "//" + window.location.host ,
 });
 
 export default api;
